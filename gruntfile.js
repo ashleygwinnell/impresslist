c70856a3262a8a3ad2f08ac09cac3e61550c4bb5
Ashley Gwinnell <info@ashleygwinnell.co.uk>
2018-10-25 15:11:26 +0100

    Push live scripts using Grunt.

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	/*
+		PRE:
+			nvm use 6
+			npm i -g grunt-cli
+			npm i
+
+		1. You need to make a file called .ftphost with data as follows:
+			{
+				"impress_stage": {
+					"host": "ftp.yourdomain.com",
+					"port": 21,
+				}
+			}
+
+		2. You need to make a file called .ftppass with data as follows:
+			{
+				"impress_stage": {
+					"username": "your_username_here",
+					"password": "your_password_here"
+				}
+			}
+			For SFTP follow docs here: https://www.npmjs.com/package/grunt-sftp-deploy
+
+		nb. You can also choose to use impress[] locally only.
+	*/
+
+	var authKey = "impress_stage";
+	var ftphostdata = grunt.file.readJSON(".ftphost")[authKey];
+	grunt.initConfig({
+		'ftp-deploy': {
+			deploy: {
+				auth: {
+					host: ftphostdata['host'],
+					port: ftphostdata['port'],
+					authKey: authKey
+				},
+				src: '.',
+				dest: ftphostdata['path'],
+				exclusions: [""],
+				forceVerbose: true
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-sftp-deploy');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+
+	grunt.registerTask('alldone', function() {
+		grunt.log.writeln('Complete at: '+ new Date().toTimeString());
+	});
+
+	grunt.registerTask('default', ["deploy"]);
+	grunt.registerTask('deploy', ['ftp-deploy:deploy', 'alldone']);
+};
